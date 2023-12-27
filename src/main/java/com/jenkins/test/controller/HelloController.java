dbbaeb680b9e7ff677e37eb6c74f69c88960a3de
eunju <eunju7757@gmail.com>
2023-12-27 11:20:23 +0900

    first commit

---
+++ src/main/java/com/jenkins/test/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.jenkins.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home(){
+        return "HOME";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello";
+    }
+}
